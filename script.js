c7cfb86e4a7616f2e6e5f4ba00d6793bdf9d7f8f
Marcus Nelhams <114110016+MarcusNelhams@users.noreply.github.com>
2024-04-19 12:43:58 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import {OBJLoader} from "three/addons/loaders/OBJLoader.js"
 // Call main function when DOM content is loaded
 document.addEventListener('DOMContentLoaded', main);
 
@@ -13,29 +14,69 @@ function main() {
     const near = 0.1;
     const far = 5;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 2; // move camera back to look at origin
 
-    camera.position.z = 2; // move camera back
-
-    // scene and box 
+    // scene
     const scene = new THREE.Scene();
+
+    // load in windmill
+    const objLoader = new OBJLoader();
+	objLoader.load( 'windmill.obj', ( root ) => {
+		scene.add( root );
+	} );
+
+    // defining box geometry
     const boxWidth = 1;
     const boxHeight = 1;
     const boxDepth = 1;
     const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
-    const material = new THREE.MeshPhongMaterial({color: 0x44aa88});
 
-    // cube
-    const cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
+    // texture loader
+    const loader = new THREE.TextureLoader();
+    const texture = loader.load("wall.jpg");
+    texture.colorSpace = THREE.SRGBColorSpace;
+
+    // simple cube generator
+    function makeInstance( geometry, color, x ) {
+
+		const material = new THREE.MeshPhongMaterial( { color } );
+
+		const cube = new THREE.Mesh( geometry, material );
+		scene.add( cube );
+
+		cube.position.x = x;
+
+		return cube;
+
+	}
+
+    // push middle, left, and right cube
+    const cubes = [
+        // makeInstance( geometry, 0x44aa88, 0 ), middle cube now textured below
+		makeInstance( geometry, 0x8844aa, - 2 ),
+		makeInstance( geometry, 0xaa8844, 2 ),
+    ]
+
+    // textured cube
+    const material = new THREE.MeshPhongMaterial( {
+		map: texture
+	} );
+    const cube = new THREE.Mesh( geometry, material );
+	scene.add( cube );
+	cubes.push( cube ); // add to our list of cubes to rotate
 
     // render
-    renderer.render(scene, camera);
-
     function render(time) {
         time *= 0.001;  // convert time to seconds
        
-        cube.rotation.x = time;
-        cube.rotation.y = time;
+        cubes.forEach( ( cube, ndx ) => {
+
+			const speed = 1 + ndx * .1;
+			const rot = time * speed;
+			cube.rotation.x = rot;
+			cube.rotation.y = rot;
+
+		} );
        
         renderer.render(scene, camera);
        
@@ -49,4 +90,5 @@ function main() {
     const light = new THREE.DirectionalLight(color, intensity);
     light.position.set(-1, 2, 4);
     scene.add(light);
+    
 }
