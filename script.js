9e4ebb5a11a7bcbce28acf675b6131d0361b64cf
Marcus Nelhams <114110016+MarcusNelhams@users.noreply.github.com>
2024-04-20 19:54:55 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,19 +9,20 @@ function main() {
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
 
     // camera
-    const fov = 75;
-    const aspect = 2; // canvas default
+    const fov = 45;
+    const aspect = 1; // canvas default
     const near = 0.1;
-    const far = 5;
+    const far = 100;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.z = 2; // move camera back to look at origin
+    camera.position.z = 20;
+    camera.position.y = 5
 
     // scene
     const scene = new THREE.Scene();
 
     // load in windmill
     const objLoader = new OBJLoader();
-	objLoader.load( 'windmill.obj', ( root ) => {
+	objLoader.load( 'windmill_001.obj', ( root ) => {
 		scene.add( root );
 	} );
 
@@ -53,8 +54,8 @@ function main() {
     // push middle, left, and right cube
     const cubes = [
         // makeInstance( geometry, 0x44aa88, 0 ), middle cube now textured below
-		makeInstance( geometry, 0x8844aa, - 2 ),
-		makeInstance( geometry, 0xaa8844, 2 ),
+		makeInstance( geometry, 0x8844aa, - 5 ),
+		makeInstance( geometry, 0xaa8844, 5 ),
     ]
 
     // textured cube
@@ -62,22 +63,39 @@ function main() {
 		map: texture
 	} );
     const cube = new THREE.Mesh( geometry, material );
+    cube.position.x = -5
+    cube.position.y = 2
 	scene.add( cube );
 	cubes.push( cube ); // add to our list of cubes to rotate
+    
+
+    function resizeRendererToDisplaySize( renderer ) {
+
+		const canvas = renderer.domElement;
+		const width = canvas.clientWidth;
+		const height = canvas.clientHeight;
+		const needResize = canvas.width !== width || canvas.height !== height;
+		if ( needResize ) {
+
+			renderer.setSize( width, height, false );
+
+		}
+
+		return needResize;
+
+	}
 
     // render
-    function render(time) {
-        time *= 0.001;  // convert time to seconds
+    function render() {
        
-        cubes.forEach( ( cube, ndx ) => {
+        if ( resizeRendererToDisplaySize( renderer ) ) {
 
-			const speed = 1 + ndx * .1;
-			const rot = time * speed;
-			cube.rotation.x = rot;
-			cube.rotation.y = rot;
+			const canvas = renderer.domElement;
+			camera.aspect = canvas.clientWidth / canvas.clientHeight;
+			camera.updateProjectionMatrix();
 
-		} );
-       
+		}
+
         renderer.render(scene, camera);
        
         requestAnimationFrame(render);
